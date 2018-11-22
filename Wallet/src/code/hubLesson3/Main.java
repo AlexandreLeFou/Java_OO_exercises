43578515d5cc5dc061222ad047d78fb50c614f68
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-22 12:25:26 +0200

    withdraw ok, need to fix deposit option

--- Wallet/src/code/hubLesson3/Main.java
+++ Wallet/src/code/hubLesson3/Main.java
@@ -16,12 +16,12 @@ public class Main {
     public static void main(String[] args) {
 
      Wallet wallet1 = new Wallet(500);//created a wallet with initially: 500$
-     Person customer1  = new Person("GordonGeko");
+     Person customer1  = new Person("GordonGeko",55);
 
 
-     wallet1.depositAmmount(50f);
+     wallet1.depositAmmount(customer1.getProductPrice());
      System.out.println("The wallet now has:"+ wallet1.getTotalAccount()+"$");
-     wallet1.withdrawAmmount(4444);
+     wallet1.withdrawAmmount(customer1.getProductPrice());
      System.out.println("The wallet now has:"+ wallet1.getTotalAccount()+"$");
 
 
--- Wallet/src/code/hubLesson3/Person.java
+++ Wallet/src/code/hubLesson3/Person.java
@@ -3,15 +3,35 @@ package code.hubLesson3;
 
 public class Person {
     private String name;
+    private float productPrice;
 
 
     //constructor
-    public Person(String name) {
+
+    public Person(String name, float productPrice) {
         this.name = name;
+        this.productPrice = productPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(float productPrice) {
+        this.productPrice = productPrice;
     }
 
     /**
      * @name: name of a Person
+     * @productPrice: the price of an item he wants to buy
      */
 
 
