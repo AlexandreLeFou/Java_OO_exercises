d39e82c82ac0e94db0dd1e5688dce1a4c50f6325
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-23 13:44:03 +0200

    testes char array for each field. fixed not practical

--- code.hub/project1/MytestProject/CSVparser/src/codeHubAssignement/Main.java
+++ code.hub/project1/MytestProject/CSVparser/src/codeHubAssignement/Main.java
@@ -11,7 +11,13 @@ public class Main {
         int c=0; //counter
         String inputString = null;
         String[] totalLines = new String[22]; //value altered according to each csv File(i.e.lines)
-      //  String[] totalLinesSplit = new String[22]; //value altered according to each csv File(i.e.lines)
+        char[] Csv1FieldName = new char[22]; //value altered according to each csv File(i.e.lines)
+        char[] Csv2FieldCarPlate = new char[22]; //value altered according to each csv File(i.e.lines)
+        char[] Csv3FieldCarModel = new char[22]; //value altered according to each csv File(i.e.lines)
+        char[] Csv4FieldCarDate = new char[22]; //value altered according to each csv File(i.e.lines)
+        char[] Csv5FieldCarFine = new char[22]; //value altered according to each csv File(i.e.lines)
+
+        //  String[] totalLinesSplit = new String[22]; //value altered according to each csv File(i.e.lines)
 
         File filetoOpen = new File ("example.csv");
         try {
@@ -20,29 +26,26 @@ public class Main {
             try{
                 //later on for option 1 for csv 2 for batabase
             System.out.println("Hello my friend please press 1 to to continue:");
-            int userInput2 = scannerInput.nextInt();
+            //int userInput2 = scannerInput.nextInt();
             }catch (InputMismatchException exception){
             System.out.print("Wrong Input");
             }
 
             System.out.println("At first when I open the csv I store it in String format:");
-
-
-            
-            while(scanner.hasNextLine()){//while it has a line to read repeat...
+             while(scanner.hasNextLine()){//while it has a line to read repeat...
                 inputString = scanner.nextLine();
                 totalLines[c] = inputString;
                 String [] CsvSplit = inputString.split(",");
-                char [] Csv1FieldName =  CsvSplit[0].toCharArray();
+                Csv1FieldName[c] =  CsvSplit[0].toCharArray();
                 //System.out.println(Csv1FieldName);
-                char [] Csv2FieldCarPlate = CsvSplit[1].toCharArray();
-                System.out.println(Csv2FieldCarPlate);
+                Csv2FieldCarPlate = CsvSplit[1].toCharArray();
+                //System.out.println(Csv2FieldCarPlate);
                // System.out.print("wwwwwww"+Csv2FieldCarPlate[c]);
-                char [] Csv3FieldCarModel = CsvSplit[2].toCharArray();
+                Csv3FieldCarModel = CsvSplit[2].toCharArray();
                 //System.out.println(Csv3FieldCarModel);
-                char [] Csv4FieldCarDate = CsvSplit[3].toCharArray();
+                Csv4FieldCarDate = CsvSplit[3].toCharArray();
                 //System.out.println(Csv4FieldCarDate);
-                char [] Csv5FieldCarFine = CsvSplit[4].toCharArray();
+                Csv5FieldCarFine = CsvSplit[4].toCharArray();
                 //System.out.println(Csv5FieldCarFine);
 
                 //char[] charGuessWord = guessword.toCharArray();
@@ -59,7 +62,7 @@ public class Main {
                 c++;
             // totalLines[c].split(",");
               }
-
+            System.out.println(Csv1FieldName[0]);
             System.out.println("\n===//===\n"); //beautiful command line separator
 
 
