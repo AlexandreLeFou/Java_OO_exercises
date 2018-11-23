9fb323e959bd2079b0c968ef21805f10497f2b7c
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-23 13:12:39 +0200

    added char array

--- code.hub/project1/MytestProject/CSVparser/src/codeHubAssignement/Main.java
+++ code.hub/project1/MytestProject/CSVparser/src/codeHubAssignement/Main.java
@@ -11,6 +11,7 @@ public class Main {
         int c=0; //counter
         String inputString = null;
         String[] totalLines = new String[22]; //value altered according to each csv File(i.e.lines)
+      //  String[] totalLinesSplit = new String[22]; //value altered according to each csv File(i.e.lines)
 
         File filetoOpen = new File ("example.csv");
         try {
@@ -26,17 +27,44 @@ public class Main {
 
             System.out.println("At first when I open the csv I store it in String format:");
 
+
+            
             while(scanner.hasNextLine()){//while it has a line to read repeat...
                 inputString = scanner.nextLine();
                 totalLines[c] = inputString;
-                System.out.println("->totalLines["+c+"]"+totalLines[c]);
+                String [] CsvSplit = inputString.split(",");
+                char [] Csv1FieldName =  CsvSplit[0].toCharArray();
+                //System.out.println(Csv1FieldName);
+                char [] Csv2FieldCarPlate = CsvSplit[1].toCharArray();
+                System.out.println(Csv2FieldCarPlate);
+               // System.out.print("wwwwwww"+Csv2FieldCarPlate[c]);
+                char [] Csv3FieldCarModel = CsvSplit[2].toCharArray();
+                //System.out.println(Csv3FieldCarModel);
+                char [] Csv4FieldCarDate = CsvSplit[3].toCharArray();
+                //System.out.println(Csv4FieldCarDate);
+                char [] Csv5FieldCarFine = CsvSplit[4].toCharArray();
+                //System.out.println(Csv5FieldCarFine);
+
+                //char[] charGuessWord = guessword.toCharArray();
+                //System.out.println("WOW"+fineCsvField);
+                //System.out.println("WOW"+CsvSplit[3]);
+
+
+        //        totalLinesSplit[c] = String.valueOf(totalLines[c].split(",", 5));
+
+
+               //System.out.println("->totalLines["+c+"] = "+totalLines[c]);
+                //System.out.println("->totalLinesSplit["+c+"] = "+totalLinesSplit[c]);
+
                 c++;
             // totalLines[c].split(",");
               }
 
             System.out.println("\n===//===\n"); //beautiful command line separator
 
-            System.out.println();
+
+
+//            System.out.println(totalLines[0].split(","));
 
 
 
