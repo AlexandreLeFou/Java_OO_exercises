0714078418874489b73ea5585a3031e6eed201d7
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-25 20:13:15 +0200

    first commit

---
+++ DdInfo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    //db steps: connection,statements,query_execution
+    //process ResultSet object, close connection
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
