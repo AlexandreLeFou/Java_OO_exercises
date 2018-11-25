79e8e1136e3eb516c3b70359023b3f9c92288291
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-25 20:26:58 +0200

    java project

--- DdInfo/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    //db steps: connection,statements,query_execution
-    //process ResultSet object, close connection
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- other/Contact Manager/README.md
+++
@@ -1,2 +0,0 @@
-Exercise :
-This programs develops a basic Contact Manager application responsible for storing and managing all our friends' contact information (name and cellphone number).
