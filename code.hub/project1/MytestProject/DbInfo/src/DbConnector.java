883869a93806dfb75ef06a6d0af3879dc1b840ea
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-27 15:34:29 +0200

    made a method to call db_connection from main class

---
+++ code.hub/project1/MytestProject/DbInfo/src/DbConnector.java
@@ -0,0 +1,59 @@
+import java.sql.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DbConnector {
+
+    public void dbcall() throws SQLException {
+        Scanner scanner = new Scanner(System.in); //so as to select insert,update,delete
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+
+        try {
+            // connect to database- change password/username as per ur pc
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306?useSSL=false", "root", "aekara21");
+
+            System.out.println("Database connection successful!\n");
+
+            // Create a statement
+            myStmt = myConn.createStatement();
+
+            //Insert values(id is set to Auto increment so no need to add value):
+            System.out.println("Gimme me a number: 1 to Insert some predefined data(or press anything else to continue):");
+            try {
+                int userInput = scanner.nextInt();
+                if (userInput == 1) {
+                    System.out.println("I am inserting successfully ");
+                    int newValues = myStmt.executeUpdate("insert into dbproject1.car_plate_data" +
+                        "(FullName,PlateNumber,CarModel,ExpirationDatestamp)" +
+                        "values " + "('Akis Pap','ABC-1243', 'Mercedes','12-aug-2202')");
+                }
+            }catch (InputMismatchException exception) {System.out.println("Wrong Input.. I'll display all the db's records regarding\nFullname, Car Plate");}
+
+            // Execute SQL query
+            myRs = myStmt.executeQuery("SELECT * FROM dbproject1.car_plate_data;");
+
+
+            // Process result set
+            while (myRs.next()) {
+                System.out.println(myRs.getString("FullName") + ", " + myRs.getString("PlateNumber"));
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            if (myRs != null) {
+                myRs.close();
+            }
+
+            if (myStmt != null) {
+                myStmt.close();
+            }
+
+            if (myConn != null) {
+                myConn.close();
+            }
+        }
+    }
+}
--- code.hub/project1/MytestProject/DbInfo/src/Main.java
+++ code.hub/project1/MytestProject/DbInfo/src/Main.java
@@ -5,54 +5,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
-        Scanner scanner = new Scanner(System.in); //so as to select insert,update,delete
-        Connection myConn = null;
-        Statement myStmt = null;
-        ResultSet myRs = null;
-
-
+    public static void main(String[] args) {
+        DbConnector myDatabase = new DbConnector();
         try {
-            // connect to database- change password/username as per ur pc
-            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306?useSSL=false", "root", "aekara21");
-
-            System.out.println("Database connection successful!\n");
-
-            // Create a statement
-            myStmt = myConn.createStatement();
-
-            //Insert values(id is set to Auto increment so no need to add value):
-            System.out.println("Gimme me a number: 1 to Insert some predefined data:");
-            int userInput = scanner.nextInt();
-            if (userInput == 1) {
-                System.out.println("I am inserting successfully ");
-                int newValues = myStmt.executeUpdate("insert into dbproject1.car_plate_data" +
-                        "(FullName,PlateNumber,CarModel,ExpirationDatestamp)" +
-                        "values " + "('Akis Pap','ABC-1243', 'Mercedes','12-aug-2202')");
-            }
-
-            // Execute SQL query
-            myRs = myStmt.executeQuery("SELECT * FROM dbproject1.car_plate_data;");
-
-
-            // Process result set
-            while (myRs.next()) {
-                System.out.println(myRs.getString("FullName") + ", " + myRs.getString("PlateNumber"));
-            }
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        } finally {
-            if (myRs != null) {
-                myRs.close();
-            }
-
-            if (myStmt != null) {
-                myStmt.close();
-            }
-
-            if (myConn != null) {
-                myConn.close();
-            }
+            myDatabase.dbcall();
+        } catch (SQLException e) {
+            System.out.println("Something went wrong please retry and check out the db and/or csv file");
+            e.printStackTrace();
         }
     }
 }
