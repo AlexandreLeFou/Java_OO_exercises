0ea9818848919b11db183b79b45c429defe2ee47
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-27 14:13:09 +0200

    added insert into option-sql querry command

--- code.hub/project1/MytestProject/DbInfo/src/Main.java
+++ code.hub/project1/MytestProject/DbInfo/src/Main.java
@@ -1,39 +1,61 @@
 import java.sql.*;
+import java.util.Scanner;
+
+
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in); //so as to select insert,update,delete
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
 
-            Connection myConn = null;
-            Statement myStmt = null;
-            ResultSet myRs = null;
 
-            try {
-                // connect to database
-                myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306?useSSL=false", "root", "aekara21");
+        try {
+            // connect to database
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306?useSSL=false", "root", "aekara21");
 
-                System.out.println("Database connection successful!\n");
+            System.out.println("Database connection successful!\n");
 
-                // Create a statement
-                myStmt = myConn.createStatement();
+            // Create a statement
+            myStmt = myConn.createStatement();
 
-                // Execute SQL query
-                myRs = myStmt.executeQuery("SELECT * FROM dbproject1.car_plate_data;");
+            //Insert values(id is set to Auto increment so no need to add value):
+            System.out.println("Gimme me a number: 1 to Insert some predefined data:");
+            int userInput = scanner.nextInt();
+            if (userInput == 1) {
+                System.out.println("I am inserting successfully ");
+                int newValues = myStmt.executeUpdate("insert into dbproject1.car_plate_data" +
+                        "(FullName,PlateNumber,CarModel,ExpirationDatestamp)" +
+                        "values " + "('Akis Pap','ABC-1243', 'Mercedes','12-aug-2202')");
+            }
 
-                // Process result set
-                while (myRs.next()) {
-                    System.out.println(myRs.getString("FullName") + ", " + myRs.getString("PlateNumber"));
-                }
-            } catch (Exception exc) {
-                exc.printStackTrace();
-            } finally {
-                if (myRs != null) { myRs.close();}
+            // Execute SQL query
+            myRs = myStmt.executeQuery("SELECT * FROM dbproject1.car_plate_data;");
 
-                if (myStmt != null) { myStmt.close();}
 
-                if (myConn != null) {myConn.close();}
+            // Process result set
+            while (myRs.next()) {
+                System.out.println(myRs.getString("FullName") + ", " + myRs.getString("PlateNumber"));
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            if (myRs != null) {
+                myRs.close();
+            }
+
+            if (myStmt != null) {
+                myStmt.close();
+            }
+
+            if (myConn != null) {
+                myConn.close();
             }
         }
     }
+}
+
 
 
