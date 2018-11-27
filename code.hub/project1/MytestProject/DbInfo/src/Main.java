675abc1a97d6e0ab9caf2c7463829e4242391def
AlexandreLeFou <alehunter@hotmail.gr>
2018-11-27 13:06:50 +0200

    connected db with java-jdbc+fixed ssl exception

--- code.hub/project1/MytestProject/DbInfo/src/Main.java
+++ code.hub/project1/MytestProject/DbInfo/src/Main.java
@@ -1,6 +1,39 @@
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws SQLException {
+
+            Connection myConn = null;
+            Statement myStmt = null;
+            ResultSet myRs = null;
+
+            try {
+                // connect to database
+                myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306?useSSL=false", "root", "aekara21");
+
+                System.out.println("Database connection successful!\n");
+
+                // Create a statement
+                myStmt = myConn.createStatement();
+
+                // Execute SQL query
+                myRs = myStmt.executeQuery("SELECT * FROM dbproject1.car_plate_data;");
+
+                // Process result set
+                while (myRs.next()) {
+                    System.out.println(myRs.getString("FullName") + ", " + myRs.getString("PlateNumber"));
+                }
+            } catch (Exception exc) {
+                exc.printStackTrace();
+            } finally {
+                if (myRs != null) { myRs.close();}
+
+                if (myStmt != null) { myStmt.close();}
+
+                if (myConn != null) {myConn.close();}
+            }
+        }
     }
-}
+
+
