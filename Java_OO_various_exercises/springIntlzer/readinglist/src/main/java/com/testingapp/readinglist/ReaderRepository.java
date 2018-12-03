8ca3f99f98df50a86a8fe620fe745f8705cae809
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-03 16:37:55 +0200

    added signle user login-not username fixed

---
+++ Java_OO_various_exercises/springIntlzer/readinglist/src/main/java/com/testingapp/readinglist/ReaderRepository.java
@@ -0,0 +1,9 @@
+package com.testingapp.readinglist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.io.Reader;
+
+public interface ReaderRepository
+        extends JpaRepository<Reader, String> {
+}
--- Java_OO_various_exercises/springIntlzer/readinglist/src/main/java/com/testingapp/readinglist/WebSecurityConfigurerAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.testingapp.readinglist;
-
-public class WebSecurityConfigurerAdapter {
-}
