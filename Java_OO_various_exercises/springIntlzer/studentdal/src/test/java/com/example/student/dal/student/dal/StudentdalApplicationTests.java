5789f987ff27d0cf2047b350242d1d9824f6d7c9
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-04 15:45:40 +0200

    added Junits for db test(add/read values)

--- Java_OO_various_exercises/springIntlzer/studentdal/src/test/java/com/example/student/dal/student/dal/StudentdalApplicationTests.java
+++ Java_OO_various_exercises/springIntlzer/studentdal/src/test/java/com/example/student/dal/student/dal/StudentdalApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.student.dal.student.dal;
 
+import com.example.student.dal.student.dal.entitities.Student;
+import com.example.student.dal.student.dal.repos.StudentRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class StudentdalApplicationTests {
 
-    @Test
+    /*@Test
     public void contextLoads() {
     }
+*/
 
+    @Autowired //create an instance of this interface at runtime
+    private  StudentRepository studentRepository;
+    @Test
+    public void testCreateStudent(){
+     Student student = new Student();
+     student.setName("John");
+     student.setCourse("Java Web Services");
+     student.setFee(30d);
+     studentRepository.save(student); //will create a db record via creation of a insert command
+
+    }
+
+    @Test
+    public void testFindStudentById(){
+        Student student = studentRepository.findOne(1l);
+        System.out.println(student);
+    }
 }
