0e487ad9890966956ca6dd15742bf49815c2ee0c
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-04 13:48:28 +0200

    added owner and class cardb entries

--- Java_OO_various_exercises/springIntlzer/CardBdProject/src/main/java/com/packt/cardatabase/domain/Car.java
+++ Java_OO_various_exercises/springIntlzer/CardBdProject/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -14,7 +14,7 @@ public class Car {
     public Car() {}
 
     public Car(String brand, String model, String color,
-               String registerNumber, int year, int price) {
+               String registerNumber, int year, int price, Owner owner) {
         super();
         this.brand = brand;
         this.model = model;
@@ -22,7 +22,7 @@ public class Car {
         this.registerNumber = registerNumber;
         this.year = year;
         this.price = price;
-
+        this.owner = owner;
     }
     public String getBrand() {
         return brand;
