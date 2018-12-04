e8aaa40b20ebf72e377396a7190cc8bafeb151a5
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-04 12:13:57 +0200

    added command line runner

--- Java_OO_various_exercises/springIntlzer/CardBdProject/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ Java_OO_various_exercises/springIntlzer/CardBdProject/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,4 +1,6 @@
 package com.packt.cardatabase.domain;
 
-public class CarRepository {
-}
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository <Car, Long> {}
--- Java_OO_various_exercises/springIntlzer/CardBdProject/src/main/java/dbcarsprojectpackt/demo/DemoDbprojectApplication.java
+++ Java_OO_various_exercises/springIntlzer/CardBdProject/src/main/java/dbcarsprojectpackt/demo/DemoDbprojectApplication.java
@@ -1,13 +1,27 @@
 package dbcarsprojectpackt.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoDbprojectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DemoDbprojectApplication.class, args);
     }
+
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            // Place your code here
+        };
+    }
 }
+
+
 // //  com.packt.cardatabase.domain
