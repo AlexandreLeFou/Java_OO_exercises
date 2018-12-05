758713ac7ce9cbee92fd7d3c77f262c449c7f601
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-05 13:59:25 +0200

    return message of suceessfull login entry back to the user via the jsp]

--- Java_OO_various_exercises/springIntlzer/locationweb/src/main/java/locationweb/locationweb/controllers/LocationController.java
+++ Java_OO_various_exercises/springIntlzer/locationweb/src/main/java/locationweb/locationweb/controllers/LocationController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import locationweb.locationweb.Service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,9 +24,11 @@ public class LocationController {
     }
 
     @RequestMapping("/saveLoc") //return the same jsp page but display the message at the end(sucess message)
-    public String saveLocation(@ModelAttribute("location") Location location) {
+    public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
         //model attribut(retreive an object)->create bean
-        service.saveLocation(location);
+        Location locationSaved = service.saveLocation(location);
+        String msg="Location saved with id:"+locationSaved.getId();
+        modelMap.addAttribute("msg", msg); //see .jsp file ${} variable ;)
         return "createLocation";
     }
 
